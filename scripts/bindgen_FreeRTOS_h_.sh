4460f16d2dea7b40f51e3170e3a22095b538e57c
lowlevelengineer <lowlevelengineer@gmail.com>
2024-05-04 17:00:16 +0900

    修正

--- scripts/bindgen_FreeRTOS_h_.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# --以下はclangのオプション
-# https://solid.kmckk.com/SOLID/doc/latest/solid_toolchain/overview.html
-bindgen ./freertos/FreeRTOS/FreeRTOS/Source/include/FreeRTOS.h -- -I/usr/include -I./ -I./freertos/FreeRTOS/FreeRTOS/Source/portable/GCC/ARM_CM3 > ./FreeRTOS.rs
--- scripts/bindgen_task_h.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# --以下はclangのオプション
-# https://solid.kmckk.com/SOLID/doc/latest/solid_toolchain/overview.html
-bindgen ./freertos/FreeRTOS/FreeRTOS/Source/include/task.h -- --include ./freertos/FreeRTOS/FreeRTOS/Source/include/FreeRTOS.h -I/usr/include -I./ -I./freertos/FreeRTOS/FreeRTOS/Source/portable/GCC/ARM_
-CM3 > ./bindings/task.rs
